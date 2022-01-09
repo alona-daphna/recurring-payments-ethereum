160139b80916663a28ecfc95cfa693bca63ef38e
alona-daphna <alona.daphna@gmail.com>
2022-01-09 09:58:12 +0200

    last commit before fronend

--- truffle-config.js
+++ truffle-config.js
@@ -1,9 +1,21 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const mnemonic = process.env.MNEMONIC;
+const infura = process.env.INFURA_KEY;
 module.exports = {
+  contracts_build_directory: './src/contracts',
+
   networks: {
     development: {
       host: "127.0.0.1",
-      port: 9545,
+      port: 8545,
       network_id: "*"
+    },
+    rinkeby: {
+      provider: () => new HDWalletProvider(
+        mnemonic,
+        `https://rinkeby.infura.io/v3/${infura}`
+      ),
+      network_id: 4
     }
   },
   compilers: {
