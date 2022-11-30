29012ff6927c02f998b5cb8dc260465e31a83da8
alona <alona.daphna@gmail.com>
2022-11-30 10:55:16 +0200

    deployed contract

--- truffle-config.js
+++ truffle-config.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const HDWalletProvider = require('@truffle/hdwallet-provider');
 const {INFURA_API_KEY, MNEMONIC} = process.env;
 
@@ -7,22 +8,12 @@ module.exports = {
   networks: {
     development: {
       host: "127.0.0.1",
-      port: 8545,
+      port: 9545,
       network_id: "*"
     },
-    rinkeby: {
-      provider: () => new HDWalletProvider(
-        mnemonic,
-        `https://rinkeby.infura.io/v3/${infura}`
-      ),
-      network_id: 4
-    },
     goerli: {
-      provider: () => new HDWalletProvider(
-        MNEMONIC,
-        INFURA_API_KEY
-      ),
-      network_id: 5
+      provider: () => new HDWalletProvider(MNEMONIC, 'https://goerli.infura.io/v3/'+INFURA_API_KEY),
+      network_id: '5'
     }
   },
   compilers: {
